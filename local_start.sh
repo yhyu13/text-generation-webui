308852a24333902ab484cbc9fc1a492715e2a644
yhyu13 <yohan680919@gmail.com>
2023-05-13 23:47:20 +0800

    Update local start script

--- local_start.sh
+++ local_start.sh
@@ -1,7 +1,8 @@
 #！bin/bash
 
 export PRE_CLI_ARGS="/usr/bin/env"
-export CLI_ARGS="--model gpt4-x-vicuna-13B-GPTQ --model_type llama --wbits 4 --groupsize 128 --chat --xformers --sdp-attention"
+export CLI_ARGS="--model chimera-inst-chat-13b-gptq-4bit --model_type llama --wbits 4 --groupsize 128 --listen --chat --xformers --sdp-attention"
+#export CLI_ARGS="--model gpt4-x-vicuna-13B-GPTQ --model_type llama --wbits 4 --groupsize 128 --chat --xformers --sdp-attention"
 #export CLI_ARGS="--model stable-vicuna-13B-GPTQ --model_type llama --wbits 4 --groupsize 128 --chat --xformers --sdp-attention"
 #export CLI_ARGS="--model llama-30b-supercot-4bit-128g-cuda --model_type llama --wbits 4 --groupsize 128 --chat --xformers --sdp-attention"
 #export CLI_ARGS="--model OpenAssistant-SFT-7-Llama-30B-GPTQ --model_type llama --wbits 4 --groupsize 128 --chat --xformers --sdp-attention"
